1eb8b14ef7020c99630e6a97e813a5eaf90452e5
riya pathan <sadullak18@gmail.com>
2026-09-06 18:28:15 +0530

    finished

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client
+import streamlit as st
+
 
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets.get("SUPABASE_URL", os.getenv("SUPABASE_URL"))
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", os.getenv("SUPABASE_KEY"))
 
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("Supabase URL or Key missing in .env")
+    raise ValueError("Supabase URL or Key missing in Streamlit Secrets")
 
 supabase = create_client(
     SUPABASE_URL,
