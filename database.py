99cdfc4989b6058be8787213e8d015c297c5ea5d
riya pathan <sadullak18@gmail.com>
2026-09-06 18:30:22 +0530

    finished

--- database.py
+++ database.py
@@ -5,6 +5,11 @@ import streamlit as st
 
 
 
+import os
+import streamlit as st
+from supabase import create_client
+from dotenv import load_dotenv
+
 load_dotenv()
 
 SUPABASE_URL = st.secrets.get("SUPABASE_URL", os.getenv("SUPABASE_URL"))
@@ -13,10 +18,7 @@ SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", os.getenv("SUPABASE_KEY"))
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Supabase URL or Key missing in Streamlit Secrets")
 
-supabase = create_client(
-    SUPABASE_URL,
-    SUPABASE_KEY
-)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def create_complaint(data):
     response = (
